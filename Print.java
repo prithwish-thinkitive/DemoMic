7519295c910290b7a469a3b29bd838735a832384
Prithwish Das <prithwishdas60@gmail.com>
2022-11-04 21:18:06 +0530

    Third print commit

--- Print.java
+++ Print.java
@@ -2,5 +2,6 @@ class Print {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("New Print");
+        System.out.println("Second Print");
     }
 }
